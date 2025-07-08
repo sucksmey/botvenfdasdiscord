5789682e531dd3926148fecd7f2cd575e8d18338
sucksmey <95987947+sucksmey@users.noreply.github.com>
2025-07-08 19:13:29 -0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ GUILD_ID = 897650833888534588
 ADMIN_ROLE_ID = 1379126175317622965
 VIP_ROLE_ID = 1070823913308827678
 CLIENT_ROLE_ID = 1380201405691727923
-VIP_PRICE = 25.00 # <-- NOVO: Preço para se tornar VIP
+VIP_PRICE = 6.00 # <-- ALTERADO: Novo preço para se tornar VIP
 
 # --- IDs de Canais e Categorias ---
 CATEGORY_VENDAS_ID = 1382399986725163140
@@ -34,7 +34,18 @@ ROBUX_PRICE_PER_UNIT = 0.041
 
 # --- Banco de Dados de Produtos e Preços ---
 PRODUCTS_DATA = {
-    "Robux": {"emoji": "💎", "prices": {"100 Robux": 4.50, "200 Robux": 8.10, "300 Robux": 12.70, "400 Robux": 17.60, "500 Robux": 21.50, "600 Robux": 25.40, "700 Robux": 29.30, "800 Robux": 33.20, "900 Robux": 37.10, "1000 Robux": 41.00}, "vip_discount": {"1000 Robux": 5.00}},
+    "Robux": {
+        "emoji": "💎", 
+        "prices": {
+            "100 Robux": 4.50, "200 Robux": 8.10, "300 Robux": 12.70, "400 Robux": 17.60, 
+            "500 Robux": 21.50, "600 Robux": 25.40, "700 Robux": 29.30, "800 Robux": 33.20, 
+            "900 Robux": 37.10, "1000 Robux": 41.00
+        },
+        # <-- ALTERADO: Tabela de preços especial para VIPs
+        "vip_prices": {
+            "1000 Robux": 36.90
+        }
+    },
     "Valorant": {"emoji": "💢", "prices": {"400 VP": 19.00, "475 VP": 21.90, "505 VP": 23.00, "815 VP": 35.00, "1000 VP": 41.90, "1305 VP": 53.00, "1700 VP": 67.00, "1810 VP": 73.00, "2050 VP": 78.90, "2175 VP": 85.90, "2205 VP": 87.00, "2720 VP": 103.00, "3085 VP": 116.00, "3225 VP": 123.00, "3650 VP": 135.90, "4025 VP": 153.00, "4450 VP": 163.00}},
     "League of Legends": {"emoji": "💥", "prices": {"485 RP": 19.00, "575 RP": 21.90, "610 RP": 23.00, "1020 RP": 35.00, "1380 RP": 44.90, "1650 RP": 53.00, "1865 RP": 60.90, "2125 RP": 67.00, "2260 RP": 73.00, "2670 RP": 85.00, "2800 RP": 86.90, "3355 RP": 103.00, "3805 RP": 116.00, "4500 RP": 135.90, "5005 RP": 153.00, "5445 RP": 163.00, "5795 RP": 173.00, "6500 RP": 192.90, "6710 RP": 203.00, "11240 RP": 323.00, "13500 RP": 382.90}},
     "Free Fire": {"emoji": "🦬", "prices": {"100 Diamantes + 10% Bônus": 6.49, "310 Diamantes + 10% Bônus": 15.99, "520 Diamantes + 10% Bônus": 22.99, "1060 Diamantes + 10% Bônus": 46.99, "2180 Diamantes + 10% Bônus": 89.99, "5600 Diamantes + 10% Bônus": 211.99}},
