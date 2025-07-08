4c6fcf92fa5474847120479a88968ef00a632f21
sucksmey <95987947+sucksmey@users.noreply.github.com>
2025-07-08 12:53:54 -0300

    Update database.py

--- database.py
+++ database.py
@@ -2,37 +2,27 @@
 import os
 import logging
 from sqlalchemy import (
-    create_engine,
-    MetaData,
-    Table,
-    Column,
-    Integer,
-    String,
-    Float,
-    DateTime,
-    BigInteger
+    create_engine, MetaData, Table, Column, Integer, String, Float, DateTime, BigInteger
 )
 from sqlalchemy.engine.url import make_url
 from sqlalchemy.ext.asyncio import create_async_engine
 from datetime import datetime
 
-# Pega a URL de conexão que configuramos na Railway
 DATABASE_URL = os.getenv('DATABASE_URL')
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL não foi encontrada nas variáveis de ambiente.")
 
-# Converte a URL para funcionar com asyncpg
 async_db_url = make_url(DATABASE_URL).render_as_string(hide_password=False).replace("postgresql://", "postgresql+asyncpg://")
 engine = create_async_engine(async_db_url)
 metadata = MetaData()
 
-# Define a estrutura da nossa tabela de transações
 transactions = Table(
     'transactions',
     metadata,
     Column('id', Integer, primary_key=True),
     Column('user_id', BigInteger, nullable=False),
     Column('user_name', String(100)),
+    Column('channel_id', BigInteger, nullable=True), # <-- NOVO
     Column('product_name', String(255), nullable=False),
     Column('price', Float, nullable=False),
     Column('gamepass_link', String(255), nullable=True),
@@ -41,10 +31,10 @@ transactions = Table(
     Column('handler_admin_id', BigInteger, nullable=True),
     Column('delivery_admin_id', BigInteger, nullable=True),
     Column('payment_method', String(50), default='PIX'),
-    Column('timestamp', DateTime, default=datetime.utcnow)
+    Column('timestamp', DateTime, default=datetime.utcnow),
+    Column('closed_at', DateTime, nullable=True) # <-- NOVO
 )
 
-# Função para criar a tabela no banco de dados se ela não existir
 async def init_db():
     async with engine.begin() as conn:
         logging.info("Verificando e criando tabelas do banco de dados, se necessário...")
